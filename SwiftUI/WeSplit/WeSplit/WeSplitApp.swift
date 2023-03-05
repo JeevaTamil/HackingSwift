ffef5c5764430a89c28e3d9c9127dee63077656c
Jeeva <azhagusundaram.t@zohocorp.com>
2023-03-05 07:17:17 +0530

    [new] initial commit

---
+++ SwiftUI/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Azhagusundaram Tamil on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
