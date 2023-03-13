c6d9b6350b87ae92c25d35fc2ce067568ba798f2
Jeeva <azhagusundaram.t@zohocorp.com>
2023-03-13 20:02:13 +0530

    [day 1] basic data types

---
+++ Swift/day1.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+
+// strings
+var str: String = "Hello, Jeeva";
+print(str)
+str = "Hello, world!"
+print(str)
+
+str = """
+Hi dude \
+Good day \ 
+take care 
+"""
+
+print(str)
+
+// Int
+var age = 28
+
+
+// double
+var someDouble = 4.1387654
+print(someDouble)
+
+// String interpolation
+let details = "my name is Jeeva and my age is \(age)"
+print(details)
+
+// Type annotations ==> : String 
